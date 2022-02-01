ea832848238a75202a995a4e57f8d36bd5e59517
Renjith Devassy <renjithdevassy@gmail.com>
2022-02-01 00:01:11 +0000

    first

---
+++ Palindrome/Palindrome/Palindrome.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    public class Palindrome
+    {
+        public bool IsPalindrome(string str)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            string temp = new string(arr);
+
+
+            if(str == temp)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
